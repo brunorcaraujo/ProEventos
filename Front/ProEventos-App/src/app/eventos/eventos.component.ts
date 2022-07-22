65b16c3ec18b918f2c577a1a74e8953991db4b7e
Bruno Araújo <brunoraraujo.91@gmail.com>
2022-07-22 15:08:09 -0300

    Criação de tela de eventos.

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-eventos',
@@ -10,6 +11,19 @@ import { Component, OnInit } from '@angular/core';
 export class EventosComponent implements OnInit {
 
   public eventos: any;
+  mostrarImagem: boolean = true;
+  private _filtroLista: string = '';
+  public eventosFiltrados: any;
+
+  public get filtroLista(): string{
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
 
   constructor(private http:HttpClient) { }
 
@@ -20,10 +34,25 @@ export class EventosComponent implements OnInit {
   public getEventos(): void{
 
     this.http.get('https://localhost:5001/api/Evento').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response,
+        this.eventosFiltrados = response
+      },
       error => console.log(error)
     );
+  }
 
+  public alterarImagem(){
+    console.log(this.mostrarImagem);
+
+    this.mostrarImagem = !this.mostrarImagem;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: {tema : string}) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) != -1
+    )
   }
 
 }
